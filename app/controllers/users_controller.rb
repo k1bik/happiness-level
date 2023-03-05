046ea646c470e3a6bcf931f5fdc3e326b86e0c52
Kirill Biller <biller.k@yandex.ru>
2023-03-05 13:42:18 +0300

    Added ability to edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,38 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit_first_name edit_last_name edit_username update]
 
-  def show
+  def index
+    @users = User.all
+  end
+
+  def show; end
+
+  def edit_first_name; end
+
+  def edit_last_name; end
+
+  def edit_username; end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_path(@user) }
+        format.json { render :show, status: :ok, location: user_path(@user) }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users do
+    member do
+      get 'edit_first_name'
+      get 'edit_last_name'
+      get 'edit_username'
+    end
+  end
   # Defines the root path route ("/")
-  root "users#show"
+  root "users#index"
 end
